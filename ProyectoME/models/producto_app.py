5fc4ecacf6c6dad3a4828f3929a32ec7877dfc16
Alberto <albertolopez.alu@iespacomolla.es>
2023-01-30 10:37:16 +0100

    Changes by ciclosm

--- ProyectoME/models/producto_app.py
+++ ProyectoME/models/producto_app.py
@@ -1,1 +1,10 @@
 from odoo import fields, models
+
+class Producto_Model(models.Model):
+    _name = "producto.model"
+    _description = "La descripción del módulo"
+
+    # Campos básicos
+    name = fields.Char('Nombre', required=True)
+    code = fields.Char('Código', required=True)
+    description = fields.Text('Descripción', help='Introduce una descripción de la propiedad')
