964f2f7b1b6b1c15c2db0a2ee5ba098e7490a3e1
Alberto <albertolopez.alu@iespacomolla.es>
2023-02-02 10:23:14 +0100

    Modelo avanzado, vista avanzada

--- ProyectoME/__manifest__.py
+++ ProyectoME/__manifest__.py
@@ -1,22 +1,20 @@
 {
-    'name': "maquina.model",
-    'author': "Alberto López Gil y Javier Escolástico Egido Luz",
-    'version': '1.0',
-    'summary': 'Módulo que permite gestionar el inventario de una máquina expendedora, permitiendo reabastecer los productos.',
-    'Description': 'Módulo que permite gestionar el inventario de una máquina expendedora, permitiendo reabastecer los productos.',
+	'name': "máquinas_expendedoras",
+	'author': "Alberto López Gil y Javier Escolástico Egido Luz",
+	'version': '1.0',
+	'summary': 'Módulo que permite gestionar el inventario de una máquina expendedora, permitiendo reabastecer los productos.',
+	'Description': 'Módulo que permite gestionar el inventario de una máquina expendedora, permitiendo reabastecer los productos.',
 
-    'category': 'Generic Modules/Others',
-    'website': 'http://www.modules/Others',
-    'depends': ['base'],
-    'application': True,
-    'installable': True,
+	'category': 'Generic Modules/Others',
+	'website': 'http://www.modules/Others',
+	'depends': ['base'],
+	'application': True,
+	'installable': True,
 
-    # data files always loaded at installation
-    #'data': [
-    #    'views/view_agencia_app.xml',
-    #    'views/view_agencia_tag.xml',
-    #    'security/ir.model.access.csv'
-    #],
-    # data files containing optionally loaded demonstration data
+	# data files always loaded at installation
+	'data': [
+		'views/producto_view.xml',
+	],
+	# data files containing optionally loaded demonstration data
 
 }
--- ProyectoME/models/producto_app.py
+++ ProyectoME/models/producto_app.py
@@ -1,10 +1,19 @@
 from odoo import fields, models
 
-class Producto_Model(models.Model):
-    _name = "producto.model"
-    _description = "La descripción del módulo"
 
-    # Campos básicos
-    name = fields.Char('Nombre', required=True)
-    code = fields.Char('Código', required=True)
-    description = fields.Text('Descripción', help='Introduce una descripción de la propiedad')
+class ProductoModel(models.Model):
+	_name = "producto.model"
+	_description = "La descripción del módulo"
+
+	# Campos básicos
+	name = fields.Char('Nombre', required=True)
+	code = fields.Char('Código', required=True)
+	description = fields.Text('Descripción', help='Introduce una descripción de la propiedad')
+	category = fields.Selection([('salado', 'Salado'), ('dulce', 'Dulce'), ('bebida', 'Bebida'), ('varios', 'Varios')],
+								string='Categoría')  # TODO posible eliminación
+	sale_price = fields.Float(string='Precio de venta', required=True)
+	image = fields.Binary(string='Imagen')
+	stock = fields.Float(string='Stock', required=True)
+	supplier = fields.Many2One('res.partner', string='Proveedor')
+	state = fields.Selection([('available', 'Disponible'), ('sold_out', 'Agotado'), ('on_the_way', 'En camino')],
+							string='Estado', required=True, default='available')
