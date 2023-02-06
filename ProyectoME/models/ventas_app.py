616ab5221fc4ee5941c36967649444f679cdd665
Alberto <albertolopez.alu@iespacomolla.es>
2023-02-06 09:30:43 +0100

    Modelo ventas

---
+++ ProyectoME/models/ventas_app.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class VentasModel(models.Model):
+	_name = 'ventas.model'
+
+	name = fields.Char(string='Nombre', required=True)
+	date = fields.Date(string='Fecha de venta', default=fields.Date.today)
+	client = fields.Many2one('res.users', string='Cliente', required=True)
+	product_id = fields.Many2one('producto.model', string='Producto vendido', required=True)
+	total = fields.Float(string='Total de la venta', required=True)
